0720ff372359a1452750954873051b62739fee30
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-07 10:06:45 -0200

    Add focused style to image.

--- src/components/modules/slate-image-plugin/ImageNode.js
+++ src/components/modules/slate-image-plugin/ImageNode.js
@@ -1,17 +1,19 @@
 import React from 'react'
+import classnames from 'classnames'
+
+import './ImageNode.css'
 
 
 const ImageNode = ({ node, state, attributes }) => {
 
   const isFocused = state.selection.hasEdgeIn(node)
   const src = node.data.get('src')
-  const className = isFocused ? 'active' : null
 
   return (
     <img
       {...attributes}
       role="presentation"
-      className={className}
+      className={classnames('image-node', { 'active': isFocused })}
       src={src}
     />
   )
