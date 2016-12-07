7a6f41f43a8d6d62adf7b07d68dede1a0742b73f
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-07 12:38:48 -0200

    Move SlateJS initial editor state to a separated file.

--- src/components/SlateEditor/SlateEditor.js
+++ src/components/SlateEditor/SlateEditor.js
@@ -1,49 +1,9 @@
 import React, { Component } from 'react'
 import classnames from 'classnames'
-import { Raw } from 'slate'
 import Utils from './Utils'
 
+import initialEditorState from './initialEditorState'
 import '../assets/css/font-awesome.css'
-
-
-const initialEditorState = Raw.deserialize({
-  nodes: [
-    {
-      "data": {},
-      "kind": "block",
-      "isVoid": false,
-      "type": "paragraph",
-      "nodes": [
-        {
-          "kind": "text",
-          "ranges": [ { "kind": "range", "text": "A Hello line of text in a paragraph. ", "marks": [] } ]
-        },
-        {
-          "data": { "href": "google.com" },
-          "kind": "inline",
-          "isVoid": false,
-          "type": "link",
-          "nodes": [
-            { "kind": "text", "ranges": [ { "kind": "range", "text": "Hello", "marks": [] } ] },
-            {
-              "data": {
-                "src": "https://orig06.deviantart.net/7869/f/2009/131/9/e/zubat_icon_by_the_fry_bat.gif"
-              },
-              "kind": "inline",
-              "isVoid": true,
-              "type": "image",
-              "nodes": [
-                { "kind": "text", "ranges": [ { "kind": "range", "text": " ", "marks": [] } ] }
-              ]
-            },
-            { "kind": "text", "ranges": [ { "kind": "range", "text": "World", "marks": [] } ] }
-          ]
-        },
-        { "kind": "text", "ranges": [ { "kind": "range", "text": "", "marks": [] } ] }
-      ]
-    }
-  ]
-}, { terse: true })
 
 
 class SlateEditor extends Component {
