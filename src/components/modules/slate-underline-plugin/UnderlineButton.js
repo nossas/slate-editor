4f59a82f3b43308358edca8c8f4581a1752adde6
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-08 22:52:36 -0200

    Add active style to underline plugin button.

--- src/components/modules/slate-underline-plugin/UnderlineButton.js
+++ src/components/modules/slate-underline-plugin/UnderlineButton.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
 
-import { underlineMarkStrategy } from './UnderlineUtils'
+import { underlineMarkStrategy, hasMark } from './UnderlineUtils'
+import './UnderlineButton.css'
 
 
 const UnderlineButton = ({ state, onChange, changeState }) => (
   <button
+    className="slate-underline-plugin--button"
     onClick={e => {
       e.preventDefault()
       onChange(underlineMarkStrategy(state))
     }}
+    data-active={hasMark(state)}
   >
     <FontAwesome name="underline" />
   </button>
--- src/components/modules/slate-underline-plugin/UnderlineUtils.js
+++ src/components/modules/slate-underline-plugin/UnderlineUtils.js
@@ -1,3 +1,5 @@
+export const hasMark = state => state.marks.some(mark => mark.type === 'underline')
+
 export const underlineMarkStrategy = state => {
   return state
     .transform()
