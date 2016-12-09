3743b80fe021a7dd01e5caab923360ec673512d6
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-08 22:42:43 -0200

    Add active style to bold plugin button.

--- src/components/modules/slate-bold-plugin/BoldButton.js
+++ src/components/modules/slate-bold-plugin/BoldButton.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
 
-import { boldMarkStrategy } from './BoldUtils'
+import { boldMarkStrategy, hasMark } from './BoldUtils'
+import './BoldButton.css'
 
 
 const BoldButton = ({ state, onChange, changeState }) => (
   <button
+    className="slate-bold-plugin--button"
     onClick={e => {
       e.preventDefault()
       onChange(boldMarkStrategy(state))
     }}
+    data-active={hasMark(state)}
   >
     <FontAwesome name="bold" />
   </button>
--- src/components/modules/slate-bold-plugin/BoldUtils.js
+++ src/components/modules/slate-bold-plugin/BoldUtils.js
@@ -1,3 +1,5 @@
+export const hasMark = state => state.marks.some(mark => mark.type === 'bold')
+
 export const boldMarkStrategy = state => {
   return state
     .transform()
