147f0127ccd3bd4a2b89f5fb48313a66f2bb7c83
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-08 22:54:53 -0200

    Add active style to strikethrough plugin button.

--- src/components/modules/slate-strikethrough-plugin/StrikethroughButton.js
+++ src/components/modules/slate-strikethrough-plugin/StrikethroughButton.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import FontAwesome from 'react-fontawesome'
 
-import { strikethroughMarkStrategy } from './StrikethroughUtils'
+import { strikethroughMarkStrategy, hasMark } from './StrikethroughUtils'
+import './StrikethroughButton.css'
 
 
 const StrikethroughButton = ({ state, onChange, changeState }) => (
   <button
+    className="slate-strikethrough-plugin--button"
     onClick={e => {
       e.preventDefault()
       onChange(strikethroughMarkStrategy(state))
     }}
+    data-active={hasMark(state)}
   >
     <FontAwesome name="strikethrough" />
   </button>
--- src/components/modules/slate-strikethrough-plugin/StrikethroughUtils.js
+++ src/components/modules/slate-strikethrough-plugin/StrikethroughUtils.js
@@ -1,3 +1,5 @@
+export const hasMark = state => state.marks.some(mark => mark.type === 'strikethrough')
+
 export const strikethroughMarkStrategy = state => {
   return state
     .transform()
