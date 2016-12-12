a7c7311f254c363a7ce8879063c62b645517355c
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-12-12 01:54:30 -0200

    Refactoring of example app and exposed modules directory structure.

--- src/example/index.js
+++ src/example/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Match } from 'react-router'
-import Home from './Home'
+import Home from './pages/Home'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css'
