183feb799156572fe2d5260fe25c270ef0364027
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-25 18:32:32 -0300

    chore(link-plugin): add tooltip to the link plugin mark #23

--- src/package/plugins/slate-link-plugin/LinkNode.js
+++ src/package/plugins/slate-link-plugin/LinkNode.js
@@ -1,29 +1,59 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { hasLinks } from './LinkUtils'
 
 // FIXME: Needs to handle assets files to work with SSR
 if (require('exenv').canUseDOM) require('./LinkNode.css')
 
+class LinkNode extends Component {
+  render() {
+    const {
+      children,
+      attributes,
+      state,
+      node: { data },
+      editor: {
+        props: { readOnly }
+      }
+    } = this.props
 
-const LinkNode = ({
-  children,
-  attributes,
-  node: { data },
-  editor: {
-    props: { readOnly }
+    return (
+      <span className="link-node-container">
+        <div
+          className="link-node--tooltip"
+          contentEditable={false}
+          style={{ display: hasLinks(state) ? 'block' : 'none' }}
+        >
+          <a
+            className="tooltip--item"
+            href={data.get('href')}
+            target="_blank"
+          >
+            {data.get('href')}
+          </a>
+          |
+          <div className="tooltip--item">
+            Edit
+          </div>
+          |
+          <div className="tooltip--item">
+            Unlink
+          </div>
+        </div>
+        <a
+          {...attributes}
+          className="link-node"
+          href={data.get('href')}
+          target={data.get('target')}
+          title={data.get('title')}
+          onClick={({ target: { tagName } }) => {
+            if (readOnly && tagName === 'IMG') window.location = data.get('href')
+          }}
+        >
+          {children}
+        </a>
+      </span>
+    )
   }
-}) => (
-  <a
-    {...attributes}
-    className="link-node"
-    href={data.get('href')}
-    target={data.get('target')}
-    title={data.get('title')}
-    onClick={({ target: { tagName } }) => {
-      if (readOnly && tagName === 'IMG') window.location = data.get('href')
-    }}
-  >
-    {children}
-  </a>
-)
+}
 
 export default LinkNode
