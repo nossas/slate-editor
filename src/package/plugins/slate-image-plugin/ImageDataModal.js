54b38492da4413a5b85707303932891aad900c2a
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-26 13:02:29 -0300

    refactor(modal): split into modal stack component #23

--- src/package/plugins/slate-image-plugin/ImageDataModal.js
+++ src/package/plugins/slate-image-plugin/ImageDataModal.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import { updateInlineImage, deleteInlineImage } from './ImageUtils'
-
-// FIXME: Needs to handle assets files to work with SSR
-if (require('exenv').canUseDOM) require('./ImageDataModal.css')
+import { Modal, ModalButton, ModalContent, ModalForm } from '../../components/modal'
 
 class ImageDataModal extends Component {
   constructor(props) {
@@ -33,90 +31,82 @@ class ImageDataModal extends Component {
     const { node, state, onChange, changeModalState } = this.props
 
     return (
-      <div className="image-plugin--edit-modal-layer">
-        <div className="image-plugin--edit-modal">
-          <div className="modal--header">
-            Edit
-            <button
-              className="button--close"
-              onClick={() => changeModalState(false)}
-            />
-          </div>
-          <div className="modal--content">
-            <div className="modal--content-left">
-              <img src={node.data.get('src')} role="presentation" />
-            </div>
+      <Modal>
+        <Modal.Header
+          closeButtonAction={() => changeModalState(false)}
+        />
 
-            <div className="modal--content-right">
-              <form onSubmit={e => {
-                e.preventDefault()
-                onChange(updateInlineImage({ state, data: this.state.imageAttributes }))
-                changeModalState(false)
-              }}>
-                <div className="form-group">
-                  <label htmlFor="image-plugin--edit-title">Title</label>
+        <ModalContent>
+          <ModalContent.Left>
+            <img src={node.data.get('src')} role="presentation" />
+          </ModalContent.Left>
+
+          <ModalContent.Right>
+            <ModalForm onSubmit={e => {
+              e.preventDefault()
+              onChange(updateInlineImage({ state, data: this.state.imageAttributes }))
+              changeModalState(false)
+            }}>
+              <ModalForm.Group>
+                <label htmlFor="image-plugin--edit-title">Title</label>
+                <input
+                  id="image-plugin--edit-title"
+                  type="text"
+                  name="title"
+                  onClick={e => e.stopPropagation()}
+                  onChange={e => this.setImageAttribute(e, e.target.value)}
+                  value={this.state.imageAttributes.title}
+                />
+              </ModalForm.Group>
+
+              <ModalForm.Group>
+                <label htmlFor="image-plugin--edit-href">Link</label>
+                <input
+                  id="image-plugin--edit-href"
+                  type="text"
+                  name="href"
+                  onClick={e => e.stopPropagation()}
+                  onChange={e => this.setImageAttribute(e, e.target.value)}
+                  value={this.state.imageAttributes.href}
+                />
+              </ModalForm.Group>
+
+              <ModalForm.Group>
+                <label htmlFor="image-plugin--edit-open-external">
                   <input
-                    id="image-plugin--edit-title"
-                    type="text"
-                    name="title"
+                    id="image-plugin--edit-open-external"
+                    type="checkbox"
+                    name="openExternal"
                     onClick={e => e.stopPropagation()}
-                    onChange={e => this.setImageAttribute(e, e.target.value)}
-                    value={this.state.imageAttributes.title}
+                    onChange={e => this.setImageAttribute(e, e.target.checked)}
+                    checked={this.state.imageAttributes.openExternal}
                   />
-                </div>
+                  Open link in new tab
+                </label>
+              </ModalForm.Group>
 
-                <div className="form-group">
-                  <label htmlFor="image-plugin--edit-href">Link</label>
-                  <input
-                    id="image-plugin--edit-href"
-                    type="text"
-                    name="href"
-                    onClick={e => e.stopPropagation()}
-                    onChange={e => this.setImageAttribute(e, e.target.value)}
-                    value={this.state.imageAttributes.href}
-                  />
-                </div>
-
-                <div className="form-group">
-                  <label htmlFor="image-plugin--edit-open-external">
-                    <input
-                      id="image-plugin--edit-open-external"
-                      type="checkbox"
-                      name="openExternal"
-                      onClick={e => e.stopPropagation()}
-                      onChange={e => this.setImageAttribute(e, e.target.checked)}
-                      checked={this.state.imageAttributes.openExternal}
-                    />
-                    Open link in new tab
-                  </label>
-                </div>
-
-                <div className="container--action-buttons">
-                  <button type="submit" className="primary">Save</button>
-                  <button
-                    type="button"
-                    className="opaque"
-                    onClick={() => changeModalState(false)}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="button"
-                    className="danger"
-                    onClick={e => {
-                      e.preventDefault()
-                      e.stopPropagation()
-                      onChange(deleteInlineImage({ state }))
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+              <ModalButton.Container>
+                <ModalButton.Primary
+                  type="submit"
+                  text="Save"
+                />
+                <ModalButton.Opaque
+                  text="Cancel"
+                  onClick={() => changeModalState(false)}
+                />
+                <ModalButton.Danger
+                  text="Delete"
+                  onClick={e => {
+                    e.preventDefault()
+                    e.stopPropagation()
+                    onChange(deleteInlineImage({ state }))
+                  }}
+                />
+              </ModalButton.Container>
+            </ModalForm>
+          </ModalContent.Right>
+        </ModalContent>
+      </Modal>
     )
   }
 }
