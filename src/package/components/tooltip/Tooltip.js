ea3f235297a1f0be75d657c5a53318e9eebd9847
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-05-29 15:49:07 -0300

    feat(link-plugin): handle data with modal close #23

--- src/package/components/tooltip/Tooltip.js
+++ src/package/components/tooltip/Tooltip.js
@@ -5,16 +5,16 @@ if (require('exenv').canUseDOM) require('./Tooltip.css')
 
 const Tooltip = ({ children, className, ...props }) => (
   <div
+    {...props}
     className="tooltip--container"
     contentEditable={false}
-    {...props}
   >
     {children}
   </div>
 )
 
-Tooltip.Item = ({ children }) => (
-  <div className="tooltip--item">
+Tooltip.Item = ({ children, ...props }) => (
+  <div {...props} className="tooltip--item">
     {children}
   </div>
 )
