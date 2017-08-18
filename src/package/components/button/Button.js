f3b85c4e613090ffccbec6b8aeee04cf142db83a
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-18 12:53:07 -0300

    chore(button): fix accepts dynamic props #39

--- src/package/components/button/Button.js
+++ src/package/components/button/Button.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import {isFunction} from './../../SlateEditor/Utils'
 
-const Button = ({ children, id, onClick, className, style, type }) => (
+const Button = ({ children, id, onClick, className, style, type, ...props }) => (
   <button
     id={id}
     style={style}
     type={type}
     onClick={(e) => isFunction(onClick) && onClick(e) }
     className={className}
+    {...props}
   >
     {children}
   </button>
