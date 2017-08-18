e665c65ef602f954aa5943968144346bf90f3dc0
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-08-17 23:20:21 -0300

    fix(example): react-router router implementation fix #37

--- src/example/index.js
+++ src/example/index.js
@@ -1,21 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Match } from 'react-router'
-import Home from './pages/Home'
+import { Router } from 'react-router'
+import createBrowserHistory from 'history/createBrowserHistory'
 
+import Home from './pages/Home'
 import './index.css'
 
-const title = 'Reboo SlateJS Editor'
+const history = createBrowserHistory()
 
-const Example = () => {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        {/* Begin Routes */}
-        <Match exactly pattern="/" render={() => <Home title={title} />} />
-        {/* End Routes */}
-      </div>
-    </BrowserRouter>
-  )
-}
-
-export default Example
+export default () => (
+  <Router history={history}>
+    <Home title='Nossas - SlateJS Editor' />
+  </Router>
+)
