82ce96920923d81cba44ed87fccc65c1b81cc3f6
Alex White <awhite@rgare.com>
2017-08-24 10:59:47 -0500

    fix(clone): Fixed the clone util to not attempt to create an element from an undefined child

--- src/package/SlateEditor/__tests__/Utils.spec.js
+++ src/package/SlateEditor/__tests__/Utils.spec.js
@@ -7,6 +7,14 @@ describe('Utils', () => {
     it('returns undefined if passed children undefined', () => {
       expect(cloneElement(undefined, {})).toEqual(undefined)
     })
+
+    it('skips undefined children', () => {
+      const children = [<span></span>, undefined, <div></div>, <i></i>, undefined]
+
+      const cloned = cloneElement(children, {})
+
+      expect(cloned.length).toBe(3)
+    })
   })
 
   describe('isFunction', () => {
