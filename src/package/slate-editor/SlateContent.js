08f803648042392a2ead3478d55fcc1a132b82d4
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2017-11-28 20:24:54 -0200

    chore(editor): implements renderNode and renderMarks #50

--- src/package/slate-editor/SlateContent.js
+++ src/package/slate-editor/SlateContent.js
@@ -2,27 +2,53 @@ import React from 'react'
 import classnames from 'classnames'
 import { Editor } from 'slate-react'
 
+//
+// Nodes
+//
 import { AlignmentNode } from '../plugins/slate-alignment-plugin'
 import { EmbedNode } from '../plugins/slate-embed-plugin'
 import { GridNode, GridRowNode, GridCellNode } from '../plugins/slate-grid-plugin'
-import { ImageNode, ImageLinkNode } from '../plugins/slate-image-plugin'
+import { ImageNode } from '../plugins/slate-image-plugin'
 import { LinkNode } from '../plugins/slate-link-plugin'
-import { UnorderedListNode, OrderedListNode, ListItemNode } from '../plugins/slate-list-plugin'
+import { ListItemNode, OrderedListNode, UnorderedListNode } from '../plugins/slate-list-plugin'
+
+//
+// Marks
+//
+import { BoldMark } from '../plugins/slate-bold-plugin'
+import { ColorMark } from '../plugins/slate-color-plugin'
+import { FontFamilyMark } from '../plugins/slate-font-family-plugin'
+import { FontSizeMark } from '../plugins/slate-font-size-plugin'
+import { ItalicMark } from '../plugins/slate-italic-plugin'
+import { StrikethroughMark } from '../plugins/slate-strikethrough-plugin'
+import { UnderlineMark } from '../plugins/slate-underline-plugin'
 
 /* eslint-disable default-case */
-export const renderNode = (props) => {
+export const renderNode = props => {
   switch (props.node.type) {
     case 'alignment': return <AlignmentNode {...props} />
     case 'embed': return <EmbedNode {...props} />
     case 'grid': return <GridNode {...props} />
     case 'grid-row': return <GridRowNode {...props} />
     case 'grid-cell': return <GridCellNode {...props} />
-    case 'image': return <ImageNode {...props} />
-    case 'image-link': return <ImageLinkNode {...props} />
+    case 'image':
+    case 'image-link': return <ImageNode {...props} />
     case 'link': return <LinkNode {...props} />
-    case 'unordered-list': return <UnorderedListNode {...props} />
-    case 'ordered-list': return <OrderedListNode {...props} />
     case 'list-item': return <ListItemNode {...props} />
+    case 'ordered-list': return <OrderedListNode {...props} />
+    case 'unordered-list': return <UnorderedListNode {...props} />
+  }
+}
+
+export const renderMark = props => {
+  switch (props.mark.type) {
+    case 'bold': return <BoldMark {...props} />
+    case 'color': return <ColorMark {...props} />
+    case 'font-family': return <FontFamilyMark {...props} />
+    case 'font-size': return <FontSizeMark {...props} />
+    case 'italic': return <ItalicMark {...props} />
+    case 'strikethrough': return <StrikethroughMark {...props} />
+    case 'underline': return <UnderlineMark {...props} />
   }
 }
 /* eslint-disable default-case */
@@ -52,6 +78,7 @@ export default ({
         outerState={outerState}
         style={style}
         renderNode={renderNode}
+        renderMark={renderMark}
         {...rest}
       />
       {children}
