a5e15c138f68cf47f1985ecf6c843ea4976b6dc2
Austin Peterson <austinppeterson@gmail.com>
2018-01-11 14:17:15 -0700

    update propType to use PropType

--- src/package/plugins/slate-color-plugin/DraggableColorPicker.js
+++ src/package/plugins/slate-color-plugin/DraggableColorPicker.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import PropTypes from 'prop-types';
+import React from 'react';
 import Draggable from 'react-draggable'
 import { SketchPicker } from 'react-color'
 
