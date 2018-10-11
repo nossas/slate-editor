d3d843256e838232fe992407d59d37db56948b2a
Igor Santos <igr.exe@gmail.com>
2018-10-11 14:05:12 -0300

    fix(plugins): change strategy to pick up color and font family values

--- packages/slate-editor-color-plugin/src/DraggableColorPicker.js
+++ packages/slate-editor-color-plugin/src/DraggableColorPicker.js
@@ -3,7 +3,7 @@ import React from 'react';
 import Draggable from 'react-draggable'
 import { SketchPicker } from 'react-color'
 
-import { colorMarkStrategy } from './ColorUtils'
+import { colorMarkStrategy, hasMark, getMark } from './ColorUtils'
 // FIXME: Needs to handle assets files to work with SSR
 if (require('exenv').canUseDOM) require('./DraggableColorPicker.css')
 
@@ -12,30 +12,37 @@ const DraggableColorPicker = ({
   value,
   changeState,
   outerState: {
-    color: { rgba, showPicker },
+    color: { showPicker },
   },
   pickerDefaultPosition,
-}) => (
-  <Draggable
-    handle=".slate-color-plugin--draggable-handle"
-    defaultPosition={pickerDefaultPosition}
-    zIndex={100}
-  >
-    <div className="slate-color-plugin--draggable-handle-container">
-      <div className="slate-color-plugin--draggable-handle" />
-      <SketchPicker
-        color={rgba}
-        onChangeComplete={color => {
-          const rgbaChange = color.rgb
-          changeState({
-            value: colorMarkStrategy({ value, rgba: rgbaChange }).value,
-            color: { rgba: rgbaChange, showPicker },
-          })
-        }}
-      />
-    </div>
-  </Draggable>
-)
+}) => {
+  let rgba = { r:0, g:0, b:0, a:1 }
+  if (hasMark(value)) {
+    rgba = getMark(value).data.get('rgba')
+  }
+  
+  return (
+    <Draggable
+      handle=".slate-color-plugin--draggable-handle"
+      defaultPosition={pickerDefaultPosition}
+      zIndex={100}
+    >
+      <div className="slate-color-plugin--draggable-handle-container">
+        <div className="slate-color-plugin--draggable-handle" />
+        <SketchPicker
+          color={rgba}
+          onChangeComplete={color => {
+            const rgbaChange = color.rgb
+            changeState({
+              value: colorMarkStrategy({ value, rgba: rgbaChange }).value,
+              color: { rgba: rgbaChange, showPicker },
+            })
+          }}
+        />
+      </div>
+    </Draggable>
+  )
+}
 
 DraggableColorPicker.propTypes = {
   value: PropTypes.object.isRequired,
--- packages/slate-editor-font-family-plugin/src/FontFamilyDropdown.js
+++ packages/slate-editor-font-family-plugin/src/FontFamilyDropdown.js
@@ -2,23 +2,30 @@ import React from 'react'
 import classnames from 'classnames'
 
 import FontFamilyList from './FontFamilyList'
-import { fontFamilyMarkStrategy } from './FontFamilyUtils'
+import { fontFamilyMarkStrategy, hasMark, getMark } from './FontFamilyUtils'
 
 
-const FontFamilyDropdown = ({ value, onChange, changeState, className, style }) => (
-  <select
-    className={classnames(className)}
-    style={style}
-    onChange={({ target: { value: fontFamilyIndex } }) => {
-      onChange(fontFamilyMarkStrategy({ value, fontFamilyIndex }))
-    }}
-  >
-    {FontFamilyList.map((font, index) => (
-      <option key={`slate-font-family-${index}`} value={index}>
-        {font.name}
-      </option>
-    ))}
-  </select>
-)
+const FontFamilyDropdown = ({ value, onChange, changeState, className, style }) => {
+  let fontFamilyDefault = 0
+  if (hasMark(value)) {
+    fontFamilyDefault = getMark(value).data.get('fontFamilyIndex')
+  }
+  return (
+    <select
+      className={classnames(className)}
+      style={style}
+      onChange={({ target: { value: fontFamilyIndex } }) => {
+        onChange(fontFamilyMarkStrategy({ value, fontFamilyIndex }))
+      }}
+      value={fontFamilyDefault}
+    >
+      {FontFamilyList.map((font, index) => (
+        <option key={`slate-font-family-${index}`} value={index}>
+          {font.name}
+        </option>
+      ))}
+    </select>
+  )
+}
 
 export default FontFamilyDropdown
