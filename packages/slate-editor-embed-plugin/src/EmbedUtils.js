09cb35aed6e35f6377edfac6d76baf6406401224
Igor Santos <igr.exe@gmail.com>
2018-10-24 16:18:57 -0300

    chore(embed-plugin): insert node only fill input embed

--- packages/slate-editor-embed-plugin/src/EmbedUtils.js
+++ packages/slate-editor-embed-plugin/src/EmbedUtils.js
@@ -1,7 +1,6 @@
 export const hasEmbed = value => value.inlines.some(inline => inline.type === 'embed')
 
-export const appendEmbed = change => {
-  const embed = window.prompt('Enter the embed that you want to add.')
+export const appendEmbed = (change, embed) => {
   return change
     .insertInline({
       type: 'embed',
